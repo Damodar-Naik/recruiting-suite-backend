18469ae098be9107fff129ea4453ed8fc5447f56
Damodar-Naik <82937522+Damodar-Naik@users.noreply.github.com>
2025-11-29 16:24:02 +0530

    return INVALID_USER code for authorization

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,13 +2,16 @@ import { Request, Response, NextFunction } from 'express';
 const jwt = require('jsonwebtoken');
 
 export const verifyHRToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization?.split(' ')[1];
+    if (!req.headers?.authorization) {
+        return res.status(401).json({ error: 'Invalid or expired token', code: 'INVALID_USER' });
+    }
 
     try {
+        const token = req.headers.authorization?.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         (req as any).user = decoded;
         next();
     } catch (error) {
-        res.status(403).json({ error: 'Invalid or expired token' });
+        return res.status(401).json({ error: 'Error while validating user', code: 'INVALID_USER' });
     }
 };
